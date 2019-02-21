b0e6b9573b3ed4a6dd84d88ab7c74388b6636a50
haythemstiti <haythemstiti@gmail.com>
2019-02-21 20:06:09 +0100

    basic project structure

---
+++ gestion-societe/src/test/java/com/haythem/gestionsociete/GestionSocieteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.haythem.gestionsociete;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GestionSocieteApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
