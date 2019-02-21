d2403e7adb43cd79022713e1153f15977aa6d768
haythemstiti <haythemstiti@gmail.com>
2019-02-21 20:50:04 +0100

    updating readme file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # gestion-societe
+This a project for personal use that mimicks human resources management tools.
+It will be updated on a regular basis.
--- gestion-societe/src/main/java/com/haythem/gestionsociete/controllers/EmployeeController.java
+++ gestion-societe/src/main/java/com/haythem/gestionsociete/controllers/EmployeeController.java
@@ -1,4 +1,20 @@
 package com.haythem.gestionsociete.controllers;
 
+import com.haythem.gestionsociete.domain.Employee;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmployeeController {
+
+    private static final String FIRST_NAME = "Haythem";
+    private static final String LAST_NAME = "Stiti";
+
+    @RequestMapping("/employee")
+    public Employee getEmployee(@RequestParam(value="firstname", defaultValue=FIRST_NAME)  String firstName,
+                                @RequestParam(value="lastname", defaultValue=LAST_NAME)  String lastName) {
+        return new Employee(firstName,lastName);
+
+    }
 }
