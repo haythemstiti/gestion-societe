cf9a220fd4c4b383b9cdfca6cb21f5390efe1cbb
haythemstiti <haythemstiti@gmail.com>
2019-02-22 20:35:36 +0100

    creating a rest template to consumle quote service Fixes 3

--- gestion-societe/src/main/java/com/haythem/gestionsociete/GestionSocieteApplication.java
+++ gestion-societe/src/main/java/com/haythem/gestionsociete/GestionSocieteApplication.java
@@ -1,13 +1,26 @@
 package com.haythem.gestionsociete;
 
+import com.haythem.gestionsociete.domain.Quote;
+import javafx.application.Application;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 public class GestionSocieteApplication {
 
-    public static void main(String[] args) {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args)
+    {
         SpringApplication.run(GestionSocieteApplication.class, args);
+        RestTemplate restTemplate = new RestTemplate();
+        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random",Quote.class);
+        logger.info(quote.toString());
     }
 
 }
